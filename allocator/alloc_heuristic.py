8e2e30fcf029d736e1966f53d75933fe564a8ff7
caio-galvao <caio.r.galvao@gmail.com>
2024-10-29 11:40:01 -0300

    add: impl for alloc heuristic

--- allocator/alloc_heuristic.py
+++ allocator/alloc_heuristic.py
@@ -1,19 +1,31 @@
 def alloc(demand, prices, available_sp, market_option, res_duration):
+    instance_allocation = initiate_instance_allocation(demand)
     ordered_instance_types = sorted(prices, key=lambda x: prices[x].get_effective_hourly_rate(market_option, res_duration), reverse=True)
 
-    od_cost = 0
-
     for instance_type in ordered_instance_types:
-        if instance_type in list(demand.keys()):
+        if instance_type in demand:
             instance_demand = demand[instance_type]
-
             hourly_price = prices[instance_type].get_effective_hourly_rate(market_option, res_duration)
             
             for t in range(len(instance_demand)):
                 num_sp_instances = min(available_sp[t] // hourly_price, instance_demand[t])
-                available_sp[t] = available_sp[t] - (num_sp_instances * hourly_price)
-
                 remaining_instances = instance_demand[t] - num_sp_instances
-                od_cost += remaining_instances * prices[instance_type].on_demand
+
+                instance_allocation[market_option][instance_type][t] = num_sp_instances
+                instance_allocation['OnDemand'][instance_type][t] = remaining_instances
+
+                available_sp[t] = available_sp[t] - (num_sp_instances * hourly_price)
     
-    return od_cost
+    return instance_allocation
+
+def initiate_instance_allocation(demand):
+    instance_allocation = {'OnDemand': {}, 'RAll': {}, 'RPartial': {}, 'RNo': {}}
+    for instance_type in demand:
+        instance_demand = demand[instance_type]
+        
+        instance_allocation['RAll'][instance_type] = [0 for _ in range(len(instance_demand))]
+        instance_allocation['RPartial'][instance_type] = [0 for _ in range(len(instance_demand))]
+        instance_allocation['RNo'][instance_type] = [0 for _ in range(len(instance_demand))]
+        instance_allocation['OnDemand'][instance_type] = [0 for _ in range(len(instance_demand))]
+
+    return instance_allocation
--- allocator/allocator_main.py
+++ allocator/allocator_main.py
@@ -11,24 +11,27 @@ import alloc_knapsack
 @click.option('--res_duration',
               type=int,
               default=8760)
+@click.option('--market_option',
+              type=str,
+              default='RNo')
 @click.option('--alloc_method',
               type=int,
               default=1)
-def main(demand_path, prices_path, cost_allocation_path, output_dir, res_duration, alloc_method):
+def main(demand_path, prices_path, cost_allocation_path, output_dir, res_duration, market_option, alloc_method):
     demand = read_demand(demand_path)
     prices = read_prices(prices_path)
     cost_allocation = read_cost_allocation(cost_allocation_path)
 
-    instance_allocation, cost_allocation = allocate(demand, prices, cost_allocation, res_duration, alloc_method)
+    instance_allocation, cost_allocation = allocate(demand, prices, cost_allocation, res_duration, market_option, alloc_method)
     
     write_allocation(instance_allocation, cost_allocation, output_dir)
 
-def allocate(demand, prices, cost_allocation, res_duration, alloc_method):
-    available_sp = get_available_savings_plans(cost_allocation, res_duration)
+def allocate(demand, prices, cost_allocation, res_duration, market_option, alloc_method):
+    available_sp = get_available_savings_plans(cost_allocation, market_option, res_duration)
 
     match alloc_method:
         case 1:
-            instance_allocation = alloc_heuristic.alloc(demand, prices, available_sp, res_duration)
+            instance_allocation = alloc_heuristic.alloc(demand, prices, available_sp, market_option, res_duration)
         case 2:
             instance_allocation = alloc_knapsack.alloc(demand, prices, available_sp, res_duration)
 
@@ -36,19 +39,26 @@ def allocate(demand, prices, cost_allocation, res_duration, alloc_method):
     
     return instance_allocation, cost_allocation
 
-def get_available_savings_plans(cost_allocation, res_duration):
+def get_available_savings_plans(cost_allocation, market_option, res_duration):
     final_t = len(cost_allocation['OnDemand'])
     available_savings_plans = [0 for _ in range(final_t)]
-    for curr_t in range(final_t):
-        up_all_upfront = cost_allocation['RAll'][curr_t]
-        up_partial_upfront = cost_allocation['RPartialUp'][curr_t]
-        hr_partial_upfront = cost_allocation['RPartialHr'][curr_t]
-        hr_no_upfront = cost_allocation['RNo'][curr_t]
     
-        available_savings_plans[curr_t] += hr_no_upfront + hr_partial_upfront
+    for curr_t in range(final_t):
+        hr_value = 0
+        up_value = 0
 
+        match market_option:
+            case 'RAll':
+                up_value = cost_allocation['RAll'][curr_t]
+            case 'RPartial':
+                up_value = cost_allocation['RPartialUp'][curr_t]
+                hr_value = cost_allocation['RPartialHr'][curr_t]
+            case 'RNo':
+                hr_value = cost_allocation['RNo'][curr_t]
+    
+        available_savings_plans[curr_t] += hr_value
         for t in range(curr_t, curr_t + res_duration):
-            available_savings_plans[t] += (up_all_upfront + up_partial_upfront) / res_duration
+            available_savings_plans[t] += up_value / res_duration
 
         curr_t += 1
 
