559ada5f2df15b784d25f2e57c6a7e227678db4f
caio-galvao <caio.r.galvao@gmail.com>
2024-11-26 19:32:06 -0300

    add: experiment structure

---
+++ src/experiment/main.py
@@ -0,0 +1,13 @@
+# receive:
+    #demand
+    #prices
+    #error configs
+    #purchases
+
+#call forecast generation
+    # demand, error config
+
+#call allocator (one call for each error config)
+    # simulated forecast, purchases, prices
+
+#write a summary file
