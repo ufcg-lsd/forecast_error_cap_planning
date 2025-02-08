278c87adc79b472da6233b564d42bb04ecdd57c5
caio.galvao <caio.r.galvao@gmail.com>
2025-02-08 17:37:20 -0300

    add: finishes implementaion of experiment

--- src/experiment/set_opt_file_structure.py
+++ src/experiment/set_opt_file_structure.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import shutil
 import pandas as pd
 from src.allocator.allocator_aux import read_prices, read_demand
 
@@ -8,22 +7,28 @@ RES_DURATION = 8760
 MARKET_OPTION = 'RNo'
 
 def main():
-    path = sys.argv[1]
-
-    demand_path = sys.argv[2]
+    output_path = sys.argv[1]
+    demand_dir = sys.argv[2]
     prices_path = sys.argv[3]
-
-def set_up_structure():
-
-    demand = read_demand(demand_path)
+    
     prices = read_prices(prices_path)
+
+    for demand_path in os.listdir(demand_dir):
+        if demand_path[-4:] == '.csv':
+            scenario_name = demand_path[7:-4]
+            scenario_path = f'{output_path}/{scenario_name}'
+            set_up_scenario(prices, f'{demand_dir}/{demand_path}', scenario_path)
+
+def set_up_scenario(prices, demand_path, scenario_path):
+    os.mkdir(scenario_path)
+    demand, timestamp = read_demand(demand_path)
     families = get_families(demand)
 
     for family in families:
         instance_types = families[family]
 
-        os.mkdir(f'{path}/{family}')
-        os.mkdir(f'{path}/{family}/output')
+        os.mkdir(f'{scenario_path}/{family}')
+        os.mkdir(f'{scenario_path}/{family}/output')
 
         od_config = {'instance': [], 'hourly_price': []}
         sp_config = {'instance': [], 'hourly_price': [], 'duration': []}
@@ -36,16 +41,17 @@ def set_up_structure():
             sp_config['hourly_price'].append(get_effective_hourly_rate(prices[instance_type], MARKET_OPTION, RES_DURATION))
             sp_config['duration'].append(RES_DURATION)
         
-        sel_demand = {key: demand[key] for key in instance_types}
         
         od_config_df = pd.DataFrame(od_config)
-        od_config_df.to_csv(f'{path}/{family}/on_demand_config.csv', index=False)
+        od_config_df.to_csv(f'{scenario_path}/{family}/on_demand_config.csv', index=False)
 
         sp_config_df = pd.DataFrame(sp_config)
-        sp_config_df.to_csv(f'{path}/{family}/savings_plan_config.csv', index=False)
+        sp_config_df.to_csv(f'{scenario_path}/{family}/savings_plan_config.csv', index=False)
 
+        sel_demand = {key: demand[key] for key in instance_types}
         sel_demand_df = pd.DataFrame(sel_demand)
-        sel_demand_df.to_csv(f'{path}/{family}/total_demand.csv', index=False)
+        sel_demand_df.insert(0, 'hour', timestamp['timestamp'])
+        sel_demand_df.to_csv(f'{scenario_path}/{family}/total_demand.csv', index=False)
 
 def get_families(demand):
     families = {}
