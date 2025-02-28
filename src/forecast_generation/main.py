2920f144de4d052fb33aaf064e5c98c95b51812d
caio-galvao <caio.r.galvao@gmail.com>
2025-02-28 15:02:35 -0300

    add: more options for forecast generation

--- src/forecast_generation/main.py
+++ src/forecast_generation/main.py
@@ -13,22 +13,28 @@ class ErrorConfig:
 @click.command()
 @click.argument('demand_path', type=click.Path(exists=True))
 @click.argument('error_configs_path', type=click.Path(exists=True))
+@click.option('--error_option',
+              type=int,
+              default=1)
 @click.argument('output_dir', type=click.Path(exists=False))
-def main(demand_path, error_configs_path, output_dir):
+def main(demand_path, error_configs_path, error_option, output_dir):
     demand = pd.read_csv(demand_path)
     error_configs = read_error_configs(error_configs_path)
-    generate_error_forecasts(demand, error_configs, output_dir)
+    generate_error_forecasts(demand, error_configs, error_option, output_dir)
 
-def generate_error_forecasts(demand, error_configs, output_dir):
+def generate_error_forecasts(demand, error_configs, error_option, output_dir):
     """
     Create the demands with errors and saves them as csv files, given a list of error configs. 
     """
 
+    size = len(next(iter(demand.values())))
+    snormal = get_snormal(size)
+
     for error_config in error_configs:
-        new_demand = add_error(demand, error_config.bias_level, error_config.sd_level)
+        new_demand = add_error(demand, error_config.bias_level, error_config.sd_level, error_option, snormal)
         new_demand.to_csv(f'{output_dir}/demand_{error_config.to_string()}.csv', index=False)
 
-def add_error(demand, bias_level, sd_level):
+def add_error(demand, bias_level, sd_level, error_option, global_snormal):
     """
     Adds error to a demand, given one level of bias and one level of deviation.
     """
@@ -38,13 +44,22 @@ def add_error(demand, bias_level, sd_level):
         if instance_type in ['timestamp', 'hour']:
             new_demand[instance_type] = demand[instance_type]
         else:
-            snormal = np.random.normal(loc=0, scale=1, size=len(demand[instance_type]))
-            new_demand[instance_type] = demand[instance_type] * (1 + bias_level + sd_level * snormal)
-        
-        #new_demand[instance_type] = demand[instance_type] * (1 + (bias_level / 100) + sd_level * snormal)
-        #new_demand[instance_type] = demand[instance_type] * (1 + (bias_level / 100)) * (1 + (sd_level * snormal))
+            match error_option:
+                case 1:
+                    snormal = get_snormal(len(demand[instance_type]))
+                    new_demand[instance_type] = round(demand[instance_type] * (1 + bias_level + sd_level * snormal))
+                case 2:
+                    snormal = get_snormal(len(demand[instance_type]))
+                    new_demand[instance_type] = round(demand[instance_type] * (1 + bias_level) * (1 + (sd_level * snormal)))
+                case 3:
+                    new_demand[instance_type] = round(demand[instance_type] * (1 + bias_level + sd_level * global_snormal))
+                case 4:
+                    new_demand[instance_type] = round(demand[instance_type] * (1 + bias_level) * (1 + (sd_level * global_snormal)))
 
-    return new_demand    
+    return new_demand
+
+def get_snormal(size):
+    return np.random.normal(loc=0, scale=1, size=size)
 
 def read_error_configs(error_configs_path):
     """
