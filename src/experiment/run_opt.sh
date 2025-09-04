ada50d1d7594114e3e61aa844adc36ff75678464
caio-galvao <caio.r.galvao@gmail.com>
2025-09-04 14:31:36 -0300

    fix (run opt): skip from optimizing families that already finished the optimization

--- src/experiment/run_opt.sh
+++ src/experiment/run_opt.sh
@@ -4,6 +4,12 @@ for scenario_path in "$main_path"*/; do
     echo "Starting: $scenario_path"
     for family_path in "$scenario_path"*; do
         echo "Optimizing: $family_path"
+        
+        if [ -f "$family_path/output/result_cost.csv" ]; then
+            echo "Skipping $family_path because result_cost.csv already exists."
+            continue
+        fi
+
         docker run -v "${family_path}:/optimizer-files" -v "${family_path}/output:/optimizer-logs" registry-git.lsd.ufcg.edu.br/pedro.serey/awsome-savings:optimizer /bin/sh -c "./run_optimization.sh /optimizer-files /optimizer-logs"
     done
 done
