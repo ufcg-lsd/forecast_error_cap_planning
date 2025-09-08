fa681efdb3feef826e7d144beb5334da58c07648
caio-galvao <caio.r.galvao@gmail.com>
2025-09-08 16:27:39 -0300

    chore (results): delete uncomplete file that would plot the forecasts

--- src/result_analysis/plot_forecasts.py
+++
@@ -1,9 +0,0 @@
-# entender as novas demandas
-    # plotar para cada um dos cenários --> demanda agrupada (todas as famílias), considerando custo od
-    # ver se existe diferença no comportando de under e over forecasting quando o nível de sd muda
-
-
-def group_demand():
-
-
-def plot_demand():
