16e56a936bbc3f51428493b6e7763eb2abc28af2
caio-galvao <caio.r.galvao@gmail.com>
2025-09-12 17:45:44 -0300

    feat (results): script for summarizing the results

---
+++ src/result_analysis/summarize_results.py
@@ -0,0 +1,82 @@
+""" Summarize results of an execution.
+
+    Creates a CSV file with the results of each scenario. Contains the costs for all markets,
+    on-demand, savings plans used and savings plans idle, both absolute and relative to the 
+    base scenario.
+"""
+
+import click
+import os
+import pandas as pd
+from util.aux import read_prices
+
+def _initialize_results_summary():
+    return pd.DataFrame({
+        'bias_level': [],
+        'sd_level': [],
+        'total_cost': [],
+        'od_cost': [],
+        'sp_cost': [],
+        'sp_used': [],
+        'sp_idle': []
+    })
+
+@click.command()
+@click.argument('results_dir', type=click.Path(exists=True))
+@click.argument('output_dir', type=click.Path(exists=False))
+@click.argument('prices_path', type=click.Path(exists=False))
+def main(results_dir, output_dir, prices_path,):
+    prices = read_prices(prices_path)
+    results_df = _initialize_results_summary()
+
+    results_dir = f'{results_dir}/allocations'
+    
+    for scenario in os.listdir(results_dir):
+        scenario_path = f'{results_dir}/{scenario}'
+        results_df = get_scenario_info(scenario_path, 8760, prices, results_df)
+
+    results_df.to_csv(f'{output_dir}/summary_results.csv', index=False)
+
+def get_scenario_info(scenario_path, res_duration, prices, results_df):
+    all_markets_cost = 0
+    on_demand_cost = 0
+    savings_plans_cost = 0
+    sp_used = 0
+    sp_idle = 0
+
+    for family in os.listdir(scenario_path):
+        family_path = f'{scenario_path}/{family}'
+        cost_allocation = pd.read_csv(f'{family_path}/alloc_cost.csv')
+        total_purchases_sp = pd.read_csv(f'{family_path}/total_purchases_sp.csv')
+        alloc_instance_family = pd.read_csv(f'{family_path}/alloc_instance.csv')
+
+        on_demand_cost += sum(cost_allocation['OnDemand'])
+        savings_plans_cost += sum(total_purchases_sp['value_reserves']) * res_duration
+        all_markets_cost += on_demand_cost + savings_plans_cost
+
+        alloc_instance_sp = alloc_instance_family[alloc_instance_family['market'] == 'r_no']
+        alloc_instance_sp = alloc_instance_sp.reset_index(drop=True)
+
+        cols_inst_types = alloc_instance_sp.columns[2:]
+        for col in cols_inst_types:
+            sp_used += sum(alloc_instance_sp[col]) * prices[col].hr_no_upfront
+        
+        sp_idle += savings_plans_cost - sp_used
+    
+    results_df.loc[len(results_df)] = [
+        scenario_path.split('/')[-1].split('_')[1],
+        scenario_path.split('/')[-1].split('_')[3],
+        all_markets_cost,
+        on_demand_cost,
+        savings_plans_cost,
+        sp_used,
+        sp_idle
+    ]
+
+    return results_df
+
+# total_cost, od_cost, sp_cost 
+# sp_used, sp_idle
+
+if __name__ == '__main__':
+    main()
